1e4325d04809d81b09f774fb6244cff9fd002cc3
karolgorecki <hi@karolgorecki.pl>
2016-08-08 19:23:38 +0200

    Add tests for ProejctChartList [73.44%]

---
+++ src/components/__tests__/ProjectChartList-test.js
@@ -0,0 +1,25 @@
+import expect from 'expect';
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProjectChartList from '../ProjectChartList/ProjectChartList';
+import styles from '../ProjectChartList/ProjectChartList.css';
+
+describe('ProjectChartList', () => {
+  const mock = {
+    foo: true,
+    bar: false
+  };
+
+  const chartList = shallow(<ProjectChartList data={mock} />);
+  it('should render correctly', () => {
+    expect(chartList).toExist();
+  });
+
+  it('should pass props to children components', () => {
+    const childrenComponents = chartList.find(`.${styles.card}`);
+
+    expect(childrenComponents.at(0).children().props().data).toEqual(mock);
+    expect(childrenComponents.at(1).children().props().data).toEqual(mock);
+    expect(childrenComponents.at(2).children().props().data).toEqual(mock);
+  });
+});
