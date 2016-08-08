ae1afb0c621c8b99bf1b4f24d2fa03663943a038
karolgorecki <hi@karolgorecki.pl>
2016-08-08 17:24:05 +0200

    Add tests for ProjectCharts [70.81%]

---
+++ src/components/__tests__/ProjectChartCodeCoverage-test.js
@@ -0,0 +1,31 @@
+import expect from 'expect';
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProjectCharts from '../ProjectChartCodeCoverage/ProjectChartCodeCoverage';
+
+describe('ProjectChartCodeCoverage', () => {
+  const names = ['foo', 'bar'];
+  const coverages = [50, 10];
+  const mock = {
+    testresults: {
+      packages: [
+        { name: names[0], coverage: coverages[0] },
+        { name: names[1], coverage: coverages[1] }
+      ]
+    }
+  };
+
+  const projectCharts = shallow(<ProjectCharts data={mock} />);
+
+  it('should render correctly', () => {
+    expect(projectCharts).toExist();
+  });
+
+  it('should pass config to charts', () => {
+    const chartProps = projectCharts.find('CardMedia').children().props();
+
+    expect(projectCharts.find('CardMedia').children().props().config).toExist();
+    expect(chartProps.config.series[0].data).toEqual(coverages);
+    expect(chartProps.config.xAxis.categories).toEqual(names);
+  });
+});
