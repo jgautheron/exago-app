2ed71e244fcef23a9c176a8b02d452eed228b16e
Jonathan Gautheron <jgautheron@neverblend.in>
2016-09-26 21:27:20 +0200

    Remove menu tests

--- src/containers/__tests__/App-test.js
+++ src/containers/__tests__/App-test.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { AppPure } from '../App/App';
 
-let reponame;
-
 class FakeRouter extends React.Component {
   static childContextTypes = {
     router: React.PropTypes.object,
@@ -17,11 +15,7 @@ class FakeRouter extends React.Component {
 
   getChildContext() {
     return {
-      router: {
-        push: repo => {
-          reponame = repo;
-        }
-      },
+      router: {},
       store: {}
     };
   }
@@ -34,8 +28,6 @@ class FakeRouter extends React.Component {
 }
 
 const props = {
-  open: () => 'open',
-  close: () => 'close',
   children: <div>Hello</div>
 };
 
@@ -48,33 +40,5 @@ describe('App', () => {
 
   it('should render correctly', () => {
     expect(appWrapper).toExist();
-  });
-
-  it('should open/close menu when menu was opened', () => {
-    const appWrapperWithMenuProp = mount(
-      <FakeRouter>
-        <AppPure {...props} menu />
-      </FakeRouter>
-    ).find(AppPure).get(0);
-    expect(appWrapperWithMenuProp.handleToggle(false)).toEqual('close');
-    expect(appWrapperWithMenuProp.handleToggle(true)).toEqual('close');
-  });
-
-  it('should open/close when menu was not opened', () => {
-    const appWrapperWithMenuProp = mount(
-      <FakeRouter>
-        <AppPure {...props} menu={false} />
-      </FakeRouter>
-    ).find(AppPure).get(0);
-
-    expect(appWrapperWithMenuProp.handleToggle(false)).toEqual('close');
-    expect(appWrapperWithMenuProp.handleToggle(true)).toEqual('open');
-  });
-
-  it('menu items should redirect to good urls', () => {
-    appWrapper.find('MenuItem').get(0).props.onClick();
-    expect(reponame).toEqual('/');
-    appWrapper.find('MenuItem').get(1).props.onClick();
-    expect(reponame).toEqual('/about');
   });
 });
